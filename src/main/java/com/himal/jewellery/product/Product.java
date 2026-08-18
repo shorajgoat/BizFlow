ecfd0e15cd87df9a961351b7d535877cada4fea6
Shoraj Ghimire <shorajghm10@gmail.com>
2026-08-18 15:14:34 +0545

    Update Product.java

--- src/main/java/com/himal/jewellery/product/Product.java
+++ src/main/java/com/himal/jewellery/product/Product.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Table;
 public class Product {
 @Id
 @GeneratedValue(strategy=GenerationType.IDENTITY)
-private long id;
+private Long id;
 @Column(nullable=false)
 private String name;
 @Column(name="purchase_price",nullable=false)
