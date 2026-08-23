9537ff1299411303c35960ee74d676f951230311
Shoraj Ghimire <shorajghm10@gmail.com>
2026-08-23 23:19:34 +0545

    Create LoginRequestDto.java

---
+++ src/main/java/com/himal/jewellery/user/LoginRequestDto.java
@@ -0,0 +1,28 @@
+package com.himal.jewellery.user;
+
+public class LoginRequestDto {
+	String username;
+	String password;
+	public LoginRequestDto() {
+		
+	}
+	public LoginRequestDto(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
