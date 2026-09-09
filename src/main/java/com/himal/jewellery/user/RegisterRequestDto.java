0fc35c047bb7f875398209c8e025359e2140efcb
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-09 17:15:27 +0545

    Create RegisterRequestDto.java

---
+++ src/main/java/com/himal/jewellery/user/RegisterRequestDto.java
@@ -0,0 +1,31 @@
+package com.himal.jewellery.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequestDto {
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Role is required")
+    private String role;
+
+    public RegisterRequestDto() {}
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
