21f1ed154766726ae65c3e6cd3e906c6255932e7
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-10 15:59:12 +0545

    Create DuplicateUsernameException.java

---
+++ src/main/java/com/himal/jewellery/exception/DuplicateUsernameException.java
@@ -0,0 +1,7 @@
+package com.himal.jewellery.exception;
+
+public class DuplicateUsernameException extends RuntimeException {
+    public DuplicateUsernameException(String message) {
+        super(message);
+    }
+}
