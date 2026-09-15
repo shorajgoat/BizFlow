d952efc2ad8ba17def1911bf9a9aafcc0a1af204
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-15 18:13:25 +0545

    Update ProductService.java

--- src/main/java/com/himal/jewellery/product/ProductService.java
+++ src/main/java/com/himal/jewellery/product/ProductService.java
@@ -8,15 +8,45 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.himal.jewellery.exception.ProductNotFoundException;
+
 @Service
 public class ProductService {
 @Autowired
 private ProductRepository productRepo;
-public Product saveProduct(Product product) {
-	return productRepo.save(product);
+
+public List<ProductResponseDto>getAllProduct(){
+	return productRepo.findAll().stream()
+			.map(product->new ProductResponseDto(product))
+			.collect(Collectors.toList());
 }
-public Optional<Product> getProductById(Long Id){
-	return productRepo.findById(Id);
+public ProductResponseDto getProductById(Long id){
+	Product product=productRepo.findById(id).orElseThrow(()
+			->new ProductNotFoundException("Product of This Id doesnt Exist"));
+	return new ProductResponseDto(product);
+}
+
+public ProductResponseDto createProduct(ProductRequestDto dto) {
+	Product product=new Product
+			(dto.getName(),dto.getPurchasePrice(),dto.getPrice(),dto.getStock(),dto.getMinStock(),dto.getSource());
+	return new ProductResponseDto(productRepo.save(product));
+}
+
+public ProductResponseDto updateProduct(Long id,ProductRequestDto dto) {
+	Product product=productRepo.findById(id).orElseThrow(()->new ProductNotFoundException("Product of this Id not found"));
+	 product.setName(dto.getName());
+     product.setPurchasePrice(dto.getPurchasePrice());
+     product.setPrice(dto.getPrice());
+     product.setStock(dto.getStock());
+     product.setMinStock(dto.getMinStock());
+     product.setSource(dto.getSource());
+	return new ProductResponseDto(productRepo.save(product));
+	
+}
+
+public void deleteProduct(Long id) {
+	Product product=productRepo.findById(id).orElseThrow(()->new ProductNotFoundException("Product cant be found "));
+	productRepo.delete(product);
 }
 
 public List<String>getAllProductName(){
