6daffebeabf37d3e0d4207c7fb9f8679aee2b3a1
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-17 18:17:41 +0545

    Create Category.java

---
+++ src/main/java/com/himal/jewellery/category/Category.java
@@ -0,0 +1,35 @@
+package com.himal.jewellery.category;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.himal.jewellery.product.Product;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public List<Product> getProducts() { return products; }
+    public void setProducts(List<Product> products) { this.products = products; }
+}
