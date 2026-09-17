345a70447b3f1a7ba82c9f63dad2ab6f0ab64877
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-17 18:17:49 +0545

    Create CategoryRepository.java

---
+++ src/main/java/com/himal/jewellery/category/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.himal.jewellery.category;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
