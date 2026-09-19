8e359554762daf732ad248f60e85310d88b727fc
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-19 16:27:30 +0545

    Create BoxOrderNotFoundException.java

---
+++ src/main/java/com/himal/jewellery/exception/BoxOrderNotFoundException.java
@@ -0,0 +1,4 @@
+package com.himal.jewellery.exception;
+public class BoxOrderNotFoundException extends RuntimeException {
+    public BoxOrderNotFoundException(String message) { super(message); }
+}
