4b173221404317299b7033e8301c36329e610a60
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-19 16:22:47 +0545

    Create SupplierRepository.java

---
+++ src/main/java/com/himal/jewellery/supplier/SupplierRepository.java
@@ -0,0 +1,6 @@
+package com.himal.jewellery.supplier;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+}
