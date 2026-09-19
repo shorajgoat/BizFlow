bbc6ef6adf4144ffa227e24a19ab473a41f1ab51
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-19 16:27:39 +0545

    Create PurchaseNotFoundException.java

---
+++ src/main/java/com/himal/jewellery/exception/PurchaseNotFoundException.java
@@ -0,0 +1,4 @@
+package com.himal.jewellery.exception;
+public class PurchaseNotFoundException extends RuntimeException {
+    public PurchaseNotFoundException(String message) { super(message); }
+}
