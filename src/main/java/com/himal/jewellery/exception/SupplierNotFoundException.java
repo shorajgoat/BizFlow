b3618fc00cb341ff4cd42798f0194223d27f8319
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-19 16:27:45 +0545

    Create SupplierNotFoundException.java

---
+++ src/main/java/com/himal/jewellery/exception/SupplierNotFoundException.java
@@ -0,0 +1,5 @@
+package com.himal.jewellery.exception;
+
+public class SupplierNotFoundException extends RuntimeException {
+    public SupplierNotFoundException(String message) { super(message); }
+}
